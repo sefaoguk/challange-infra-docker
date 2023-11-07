d07fbf4f0be0f73e49dd084359ed59bde81bc925
root <root@pop-os.localdomain>
2023-11-07 14:05:39 +0300

    Değişikliklerinizi açıklayan bir mesaj

---
+++ symfony/admin/index2.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html style='background:black;color:white'>
+<body>
+<h1 style='background:#333'> PHP 2</h1>
+<?php
+$color = "red";
+echo "My boat is " . $coLOR . "<br>";
+?>
+</body>
+</html>
---
+++ symfony/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html style='background:black;color:white'>
+<body>
+<h1 style='background:#333'> PHP EXAMPLE PROJECT</h1>
+<?php
+$color = "red";
+echo "My car is " . $color . "<br>";
+echo "My house is " . $COLOR . "<br>";
+echo "My boat is " . $coLOR . "<br>";
+?>
+</body>
+</html>
